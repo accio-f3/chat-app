27ba35ad5dbd2d2c059554918a3284a0532bf3e1
accio-f3 <acciof3@gmail.com>
2023-07-20 22:26:07 +0530

    chat app done

--- public/script.js
+++ public/script.js
@@ -1,15 +1,53 @@
-let username='';
+const socket = io();
 
-document.getElementById('join-btn').addEventListener('click',(event)=>{
+let username = '';
+
+document.getElementById('join-btn').addEventListener('click', (event) => {
     event.preventDefault();
     username = document.getElementById('username-input').value;
-    if(username.trim()!==''){
-        document.querySelector('.form-username').style.display='none';
-        document.querySelector('.chatroom-container').style.display='block';
+    if (username.trim() !== '') {
+        document.querySelector('.form-username').style.display = 'none';
+        document.querySelector('.chatroom-container').style.display = 'block';
     }
 })
 
-// Trim
-// '        ' -> ''
-// '    ab c' -> 'ab c'
-// ' a b c  ' -> 'a b c'
+document.getElementById('send-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const data = {
+        username: username,
+        message: document.getElementById('message-input').value,
+    }
+    // if `io` is emitting anything `sockets` can listen
+    // if `socket` is emitting anything only `io` can listen
+    // sending message to io
+    socket.emit('message',data);
+    // what ever message i am sending i need to show this
+    // on ui for user
+    addMessageFn(data);
+})
+
+// recieving the message
+socket.on('message', (data) => {
+    // before adding this, just check if you are the sender
+    if (data.username !== username) {
+        addMessageFnRe(data);
+    }
+})
+
+// working for sent messages
+function addMessageFn(data){
+    var msgDiv = document.createElement('div');
+    msgDiv.innerText=`${data.username}: ${data.message}`;
+    msgDiv.setAttribute('class','message sent');
+    document.getElementById('message-container').appendChild(msgDiv);
+    document.getElementById('message-input').value='';
+}
+
+// working for reci messages
+function addMessageFnRe(data){
+    var msgDiv = document.createElement('div');
+    msgDiv.innerText=`${data.username}: ${data.message}`;
+    msgDiv.setAttribute('class','message recieved');
+    document.getElementById('message-container').appendChild(msgDiv);
+    document.getElementById('message-input').value='';
+}
