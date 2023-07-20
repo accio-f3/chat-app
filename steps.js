d68b98bd778d9833806a9ca7c69184e564e21bea
accio-f3 <acciof3@gmail.com>
2023-07-20 22:29:21 +0530

    cloning commands

--- steps.js
+++ steps.js
@@ -4,3 +4,7 @@
 // 3rd step is 'npm i express' -> node_modules and package-lock.json
 // 4th step is 'npm install --save-dev nodemon'
 // if 4th step is not working -> 'https://stackoverflow.com/questions/35530930/nodemon-not-working-bash-nodemon-command-not-found'
+
+// if you are cloning and running
+// 1st step is npm i
+// 2nd step nodemon server.js
